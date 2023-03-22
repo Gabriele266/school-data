a2f3a702f87811dd3101304637523404def182bb
valentinameineri <meineri.valentina@outlook.it>
2023-03-22 20:40:17 +0100

    Visualizzazione classi

---
+++ SchoolData/Migrations/20230322193853_CreateClass.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolData.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Classe = table.Column<string>(type: "TEXT", nullable: true),
+                    Specializzazione = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Class");
+        }
+    }
+}
---
+++ SchoolData/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolData.Models;
+
+public class Class
+{
+    public int Id { get; set; }
+    public string? Classe { get; set; }
+    public string? Specializzazione { get; set; }
+
+}
