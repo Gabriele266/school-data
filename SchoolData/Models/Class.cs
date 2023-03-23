f8c9dda1a672bb3139c889dda6dfa65fc644eafc
Flavio Mandrile <flavioorganizzazione@gmail.com>
2023-03-23 13:33:17 +0100

    tentativo di mock per visualizzare la media delle classi

--- SchoolData/Models/Class.cs
+++ SchoolData/Models/Class.cs
@@ -8,5 +8,5 @@ public class Class
     public int Id { get; set; }
     public string? Classe { get; set; }
     public string? Specializzazione { get; set; }
-
+    public int MediaVotiClasse { get; set; } // non so perche ho dovuto metterlo, forse devo toglierlo
 }
